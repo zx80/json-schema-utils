7d362f87a65d771a10886b16a5db1be58250581d
Fabien Coelho <fabien@coelho.net>
2025-02-02 21:46:51 +0100

    enum simplification

--- jsutils/simplify.py
+++ jsutils/simplify.py
@@ -1,3 +1,4 @@
+from typing import Any
 from .utils import JsonSchema, log
 from .recurse import recurseSchema
 
@@ -23,6 +24,22 @@ def incompatibleProps(st: str):
 # TODO string-specific formats?
 
 
+def getEnum(ls: list[JsonSchema]) -> list[Any]|None:
+    assert isinstance(ls, list)
+    lv = []
+    for s in ls:
+        if isinstance(s, dict):
+            if "const" in s:
+                lv.append(s["const"])
+            elif "enum" in s:
+                lv.extend(s["enum"])
+            else:
+                return None
+        else:
+            return None
+    return lv
+
+
 def simplifySchema(schema: JsonSchema, url: str):
 
     def rwtSimpler(schema: JsonSchema, path: list[str]) -> JsonSchema:
@@ -32,13 +49,6 @@ def simplifySchema(schema: JsonSchema, url: str):
         if isinstance(schema, bool):
             return schema
         assert isinstance(schema, dict)
-
-        if "const" in schema and "enum" in schema:
-            log.info(f"const/enum at {lpath}")
-            if schema["const"] in schema["enum"]:
-                del schema["enum"]
-            else:
-                return False
 
         if "type" in schema and isinstance(schema["type"], str):
             stype = schema["type"]
@@ -51,7 +61,39 @@ def simplifySchema(schema: JsonSchema, url: str):
                         log.info(f"unused property {p} for {stype} at {lpath}")
                         del schema[p]
 
-        # TODO switch oneOf const to enum
+        # TODO switch oneOf const/enum to enum
+        if "oneOf" in schema:
+            # TODO remove duplicates
+            lv = getEnum(schema["oneOf"])
+            if lv is not None:
+                del schema["oneOf"]
+                log.info("oneof to enum/const/false at {lpath}")
+                if len(lv) == 0:
+                    # FIXME check
+                    return False
+                else:  # at least one
+                    if "enum" in schema:
+                        lev = schema["enum"]
+                        del schema["enum"]
+                        # intersect in initial order
+                        nlv = []
+                        for v in lev:
+                            if v in lv:
+                                nlv.append(v)
+                        schema["enum"] = nlv
+                    else:
+                        schema["enum"] = lv
+
+        if "const" in schema and "enum" in schema:
+            log.info(f"const/enum at {lpath}")
+            if schema["const"] in schema["enum"]:
+                del schema["enum"]
+            else:
+                return False
+        elif "enum" in schema and len(schema["enum"]) == 1:
+            log.info(f"enum of one at {lpath}")
+            schema["const"] = schema["enum"][0]
+            del schema["enum"]
 
         return schema
 
