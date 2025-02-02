a4d6472a5ab66463e1a08ec058b118924a590dae
Fabien Coelho <fabien@coelho.net>
2025-02-02 19:35:56 +0100

    simplify props depending on type

--- jsutils/simplify.py
+++ jsutils/simplify.py
@@ -1,6 +1,26 @@
 from .utils import JsonSchema
 from .recurse import recurseSchema
 
+# type-specific properties
+# TODO complete
+TYPED_PROPS: dict[str, set[str]] = {
+    "string": {"minLength", "maxLength", "pattern"},
+    "number": {"minimum", "exclusiveMinimum", "maximum", "exclusiveMaximum", "multipleOf"},
+    "object": {"additionalProperties", "unevaluatedProperties", "patternProperties", "required",
+        "properties", "minProperties", "maxProperties"},
+    "array": {"items", "minItems", "maxItems", "prefixItems", "contains", "minContains",
+        "maxContains"},
+    "boolean": {},
+    "null": {}
+}
+
+def incompatibleProps(st: str):
+    props = set()
+    [ props := props.union(p) for t, p in TYPED_PROPS.items() if t != st ]
+    return props
+
+# TODO string-specific formats?
+
 def simplifySchema(schema: JsonSchema, url: str):
 
     def rwtSimpler(schema: JsonSchema, path: list[str]) -> JsonSchema:
@@ -13,8 +33,17 @@ def simplifySchema(schema: JsonSchema, url: str):
             if schema["const"] in schema["enum"]:
                 del schema["enum"]
             else:
-                schema = False
-        # TODO remove ignored stuff depending on type
+                return False
+
+        if "type" in schema and isinstance(schema["type"], str):
+            stype = schema["type"]
+            if stype == "integer":
+                stype = "number"
+            # remove type-specific properties
+            if stype in TYPED_PROPS:
+                for p in incompatibleProps(stype):
+                    schema.pop(p, None)
+            
         # TODO switch oneOf const to enum
 
         return schema
